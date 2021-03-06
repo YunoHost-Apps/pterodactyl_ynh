6231ad2c49716c5f2f5aab36711ebe0ab0466bf8
ericgaspar <junk.eg@free.fr>
2021-03-06 09:29:03 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,7 +10,7 @@ nodejs_version=14
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip"
 
-pkg_dependencies="libargon2-1 libsodium23 libzip5 memcached apt-transport-https software-properties-common zip unzip tar make gcc g++"
+pkg_dependencies="libargon2-1 libsodium23 apt-transport-https unzip tar make gcc g++"
 
 
 #=================================================
