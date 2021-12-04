e0d8e6b33042b56ae68c5192d29645e7b991ccb2
ericgaspar <junk.eg@free.fr>
2021-12-04 14:08:17 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,11 +7,9 @@
 # Composer version
 YNH_COMPOSER_VERSION="2.1.3"
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="8.0"
 
-nodejs_version="14"
-
-pkg_dependencies="libargon2-1 libsodium23 apt-transport-https unzip tar make gcc g++ php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip"
+pkg_dependencies="libargon2-1 libsodium23 apt-transport-https unzip tar php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip"
 
 #=================================================
 # EXPERIMENTAL HELPERS
