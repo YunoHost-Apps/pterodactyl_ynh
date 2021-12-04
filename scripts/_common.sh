732f1eb594f0379e20871171bb5c05b677d70e6e
ericgaspar <junk.eg@free.fr>
2021-12-04 13:55:31 +0100

    Update to 1.6.6

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,18 +4,15 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION=7.3
+# Composer version
+YNH_COMPOSER_VERSION="2.1.3"
 
-nodejs_version=14
+YNH_PHP_VERSION="7.3"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip"
+nodejs_version="14"
 
-pkg_dependencies="libargon2-1 libsodium23 apt-transport-https unzip tar make gcc g++"
+pkg_dependencies="libargon2-1 libsodium23 apt-transport-https unzip tar make gcc g++ php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip"
 
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
-
-#=======================
-# COMPOSER
-#=======================
